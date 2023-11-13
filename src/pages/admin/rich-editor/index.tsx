1bc79ffebc1866f1d737dd22b20f3d29fdf5216f
wongyichen <wongyichen@163.com>
2023-11-13 21:14:42 +0800

    update: layout styles

--- src/pages/admin/rich-editor/index.tsx
+++ src/pages/admin/rich-editor/index.tsx
@@ -3,7 +3,7 @@ import RichEditor from "@/components/RichEditor"
 
 function RichEditorPage() {
     return (
-        <Card className={"h-full overflow-y-auto"} title={"富文本编辑器"} bordered={false}>
+        <Card title={"富文本编辑器"} bordered={false}>
             <RichEditor></RichEditor>
         </Card>
     )
